b7240bdacbb27620d90295f619fa6bb80181459e
AlSovPich <58888340+AlSovPich@users.noreply.github.com>
2024-10-24 09:43:08 +0300

    Uploading basic version of data_engineering

--- stages/data_engineering/src/data_extraction.py
+++ stages/data_engineering/src/data_extraction.py
@@ -1,1 +1,16 @@
-# Extract data from the folder /app/data/raw or /app/data/preprocessed
+import os
+from typing import Optional, List
+from pydub import AudioSegment
+
+def extract_data(source: Optional[str] = None):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    if source == None:
+        audiofiles_dir = os.path.join(current_dir, '../../../data/raw/audiofiles')
+    else:
+        audiofiles_dir = os.path.join(current_dir, f'../../../data/{source}/audiofiles')
+    
+    dataset = {}
+    for filename in os.listdir(audiofiles_dir):
+        dataset[filename] = AudioSegment.from_file(filename)
+    
+    return dataset
--- stages/data_engineering/src/data_preparation.py
+++ stages/data_engineering/src/data_preparation.py
@@ -1,1 +1,18 @@
-# Store the preprocessed data in /app/data/preprocessed
+from pydub import AudioSegment
+from pydub.effects import normalize
+import os
+
+def process_data(data: dict):
+    normalized_data = [] 
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    audiofiles_dir = os.path.join(current_dir, '../../../data/preprocessed/audiofiles')
+
+    for filename in data.keys:
+        audio = data[filename]
+        normalized_data.append(normalize(audio))
+
+        output_file_path = os.path.join(audiofiles_dir, f"{filename}.wav")
+        audio.export(output_file_path, format='wav')
+
+    return normalized_data
