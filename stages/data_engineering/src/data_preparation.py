ba18c03ff6b9a324dc507bbad3a90160ef21ab86
AlSovPich <58888340+AlSovPich@users.noreply.github.com>
2024-10-24 16:44:31 +0300

    Add files via upload

--- stages/data_engineering/src/data_preparation.py
+++ stages/data_engineering/src/data_preparation.py
@@ -6,13 +6,14 @@ def prepare_data(data: dict, user_data: bool):
     normalized_data = [] 
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    audiofiles_dir = os.path.join(current_dir, '../../../data/preprocessed/audiofiles')
+    audiofiles_dir = os.path.join(current_dir, 'data/preprocessed/audiofiles')
 
-    for filename in data.keys:
-        audio = data[filename]
-        normalized_data.append(normalize(audio))
-        if not user_data:
-            output_file_path = os.path.join(audiofiles_dir, f"{filename}.wav")
-            audio.export(output_file_path, format='wav')
+    for folder in data.keys:
+        for filename in folder.keys:
+            audio = data[folder][filename]
+            normalized_data.append(normalize(audio))
+            if not user_data:
+                output_file_path = os.path.join(audiofiles_dir, f"{folder/filename}.wav")
+                audio.export(output_file_path, format='wav')
 
     return normalized_data
