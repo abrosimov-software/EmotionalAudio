02b20f4685282841c919114209a8dd4e5c7020a4
AlSovPich <58888340+AlSovPich@users.noreply.github.com>
2024-10-31 11:49:13 +0300

    Code rewritten, neutral-normalization implemented
    Now to avoid actor voice affecting predictions, for each actor features for neutral expression are computed and used to normalize all the other actor recordings

--- stages/data_engineering/src/feature_extraction.py
+++ stages/data_engineering/src/feature_extraction.py
@@ -2,8 +2,9 @@ import os
 import torch
 import librosa
 import numpy as np
+from microservice.src.audio_normalization import calculate_neutral_stats, normalize_features
 
-def extract_labels(audiofiles_dir):
+def extract_labels(audiofiles_dict, audiofiles_dir):
     emotion_map = {
         '01': 'neutral',
         '02': 'calm',
@@ -15,7 +16,7 @@ def extract_labels(audiofiles_dir):
         '08': 'surprised'
     }
 
-    for filename in os.listdir(audiofiles_dir):
+    for filename in audiofiles_dict.keys:
         parts = filename[:-4].split('-')
         emotion = emotion_map.get(parts[2], 'unknown')
 
@@ -24,30 +25,38 @@ def extract_labels(audiofiles_dir):
     
     return 0
 
-def extract_audio_features(audiofiles_dir):
-    for filename in os.listdir(audiofiles_dir):
-        y, sr = librosa.load(filename, sr=None)
-        
-        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-        chroma = librosa.feature.chroma_stft(y=y, sr=sr)
-        spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
-        zcr = librosa.feature.zero_crossing_rate(y)
-        rms = librosa.feature.rms(y=y)
-        tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
-        duration = librosa.get_duration(y=y, sr=sr)
-        
-        features = {
-            'mfcc_mean': np.mean(mfccs, axis=1).tolist(),
-            'mfcc_var': np.var(mfccs, axis=1).tolist(),
-            'chroma_mean': np.mean(chroma, axis=1).tolist(),
-            'chroma_var': np.var(chroma, axis=1).tolist(),
-            'spectral_contrast_mean': np.mean(spectral_contrast, axis=1).tolist(),
-            'zcr_mean': np.mean(zcr).item(),
-            'rms_mean': np.mean(rms).item(),
-            'tempo': tempo,
-            'duration': duration
-        }
+def extract_features_from_file(audio_tensor):
+    y = audio_tensor.numpy()
+    sr = 16000
 
-        tensor = torch.tensor(features)
-        torch.save(tensor, os.path.join(audiofiles_dir, f"../features/{filename}.pt"))
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+    spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    rms = librosa.feature.rms(y=y)
+    tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+    duration = librosa.get_duration(y=y, sr=sr)
+
+    features = {
+        'mfcc_mean': np.mean(mfccs, axis=1).tolist(),
+        'mfcc_var': np.var(mfccs, axis=1).tolist(),
+        'chroma_mean': np.mean(chroma, axis=1).tolist(),
+        'chroma_var': np.var(chroma, axis=1).tolist(),
+        'spectral_contrast_mean': np.mean(spectral_contrast, axis=1).tolist(),
+        'zcr_mean': np.mean(zcr).item(),
+        'rms_mean': np.mean(rms).item(),
+        'tempo': tempo,
+        'duration': duration
+    }
+
+    return features
+
+def extract_audio_features(audiofiles_dict, audiofiles_dir):
+    for filename in audiofiles_dict.keys:
+        tensor = audiofiles_dict[filename]
+        features = extract_features_from_file(tensor)
+        output_tensor = torch.tensor(features)
+
+        output_file_path = os.path.join(audiofiles_dir, f"../features/{filename}.pt")
+        torch.save(output_tensor, output_file_path)
     return 0
